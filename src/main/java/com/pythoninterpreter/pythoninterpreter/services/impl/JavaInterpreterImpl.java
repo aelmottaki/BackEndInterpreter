0a3937545f5f0e7aa6c379278fdcb582ac568431
Abdelilah ELMOTTAKI <aelmottaki@sqli.com>
2019-02-11 17:26:02 +0100

    Interpreter backend : clean code

--- src/main/java/com/pythoninterpreter/pythoninterpreter/services/impl/JavaInterpreterImpl.java
+++ src/main/java/com/pythoninterpreter/pythoninterpreter/services/impl/JavaInterpreterImpl.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
-@Component("javaInterpreterImpl")
-@Scope(scopeName = WebApplicationContext.SCOPE_SESSION,proxyMode = ScopedProxyMode.INTERFACES)
+
 public class JavaInterpreterImpl implements CodeInterpreter {
 	
 	public static final String JAVA_INTERPRETER_NOT_YES_IMPLEMENTED = "java interpreter not yes implemented !";
